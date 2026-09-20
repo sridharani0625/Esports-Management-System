d34ee852e011c12096e58db4e59af80469b6eb84
sridharani0625 <sridharanignanavel@gmail.com>
2026-09-20 16:27:45 +0530

    Connect frontend to Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://esports-management-system.onrender.com";
 
 function App() {
   const [page, setPage] = useState("login");
